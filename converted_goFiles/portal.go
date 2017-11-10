0c794c00fe6b99dbc6cba7048c4e174495995022
AmanPatelcubastion <aman.patel@cubastion.com>
2017-11-10 09:42:41 +0530

    "Aman , 8/11"

--- converted_goFiles/portal.go
+++ converted_goFiles/portal.go
@@ -2,15 +2,44 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-
+	_ "database/sql"
 	"net/http"
+	"github.com/jinzhu/gorm"
+	"log"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+
 )
 
-func main(){
-	router:=mux.NewRouter()
+var db *gorm.DB
+var err error
 
-           router.HandleFunc("/positions",fetch_position)
-           router.HandleFunc("/users",fetch_users)
-           http.ListenAndServe(":8080",router)
+func main(){
+
+	db,err=gorm.Open("mysql","root:password@/x_showroom?charset=utf8&parseTime=True&loc=Local")
+	//db,err:=gorm.Open("postgres","user=aman password=password dbname=test1 sslmode=disable")
+
+	if err!=nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.DB().Ping()
+	if err != nil {
+		log.Fatal(err)
+	}else{
+		fmt.Println("connected")
+	}
+
+	db.SingularTable(true)
+
+
+	router:=mux.NewRouter()
+	router.HandleFunc("/positions",fetch_position)
+	router.HandleFunc("/prodcat",fetch_product_categories)
+
+	router.HandleFunc("/users",fetch_users)
+	http.ListenAndServe(":8080",router)
+
 }
 
